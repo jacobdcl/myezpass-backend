9e08fdc74e68827185109c34956e84dbc8a56338
jacobdcl <98243210+jacobdcl@users.noreply.github.com>
2024-10-14 23:08:27 -0400

    cirs open to all test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: ['https://www.ezpny.vercel.app', ], // Add your frontend URL here
+  origin: 'https://ezpny.vercel.app', // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow credentials
