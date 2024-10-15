58dae399242cc8c20477a2bfb210a002951273f9
jacobdcl <98243210+jacobdcl@users.noreply.github.com>
2024-10-15 15:15:37 -0400

    this worked locally

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const crypto = require('crypto');
+const bcryptjs = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -10,22 +10,27 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
-  },
-  salt: String
+  }
 });
 
-// Hash password before saving
-userSchema.pre('save', function(next) {
+UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.salt = crypto.randomBytes(16).toString('hex');
-  this.password = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, 'sha512').toString('hex');
-  next();
+  
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-// Compare password method
-userSchema.methods.comparePassword = function(candidatePassword) {
-  const hash = crypto.pbkdf2Sync(candidatePassword, this.salt, 1000, 64, 'sha512').toString('hex');
-  return this.password === hash;
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcryptjs.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw error;
+  }
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -35,7 +35,7 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    const isMatch = user.comparePassword(password);
+    const isMatch = await user.comparePassword(password);
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
